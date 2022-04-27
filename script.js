3c50c064dc5f2ae9403ec4ef94404a9ad1cfbd2f
Mabu4 <max.budziat@gmx.de>
2022-04-27 09:34:46 +0200

    change Design, connect weather API, added search function

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
 
 
 let news = [];
+let weather = [];
 let start = 0;
 let end = 10;
 let scrollEnd = false;
-const url = 'https://www.heise.de/extras/frontend/news/';
+let cityInput = 'Berlin';
 
-window.onscroll = function(ev) {
-    if ((window.innerHeight + window.scrollY + 400) >= document.body.offsetHeight && !scrollEnd) {
-        scrollEnd = true;
-        console.log('unten');
-        if(end < 100){
-            start = start + 10;
-            end = end + 10;
-            renderNews();
-        } 
-        
-        
-        
-    }
-};
 
-async function init() {
-    let response = await fetch(url, {
+function init() {
+    initNews();
+    initWeather();
+}
+
+
+async function initNews(){
+    const url = 'https://www.heise.de/extras/frontend/news/';
+    const options = {
         headers: {
             'X-Heise-Token': 'zcJulkgE',
         }
-    });
+    };
+    let response = await fetch(url, options);
     news = await response.json();
-    // renderTopNews();
     renderNews();
     console.log('news: ', news);
 }
@@ -42,15 +36,68 @@ function renderNews(){
 
         <div class="news-element" onclick="openDetailView(${element['id']})">
             <img class="news-element-image" src="${element['image']['src']}"  alt="${element['image']['alt']}">
-            <h3 class="news-element-date">${element['meta']['pubDate']}</h3>
-            <h2 class="headline news-element-headline">${element['title']}</h2>
-            <p class="paragraph news-element-paragraph">${element['synopsis']}</p>
+            <div class="news-element-footer">
+                <h3 class="news-element-date">${element['meta']['pubDate']}</h3>
+                <h2 class="headline news-element-headline">${element['title']}</h2>
+                <p class="paragraph news-element-paragraph">${element['synopsis']}</p>
+            </div>
         </div>
         `;
     }
     scrollEnd = false;
 }
 
-function openDetailView() {
-
+async function initWeather(city){
+    if(city){
+        cityInput = city;
+    }
+    let apiKey = '9f2b47ee5924424199d215943222604';
+    let url = (`http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityInput}&lang=de`);
+    let response = await fetch(url);
+    weather = await response.json();
+    console.log('Wetter: ', weather);
+    renderWeather();
 }
+
+function renderWeather(){
+    const temps = document.getElementsByClassName('temp');
+    for (const temp of temps) {
+        temp.innerHTML = weather['current']['temp_c'];
+    }
+    document.getElementById('city').innerHTML = weather['location']['name'];
+    document.getElementById('condition').innerHTML = weather['current']['condition']['text'];
+}
+
+
+function changeCity(){
+    let selectedCity = document.getElementById('selectedCity');
+    initWeather(selectedCity.value);
+    selectedCity.value = '';
+}
+
+
+function openDetailView(id) {
+    console.log(id);
+}
+
+
+function activateLink(clickedElement){
+    const links = document.getElementsByClassName('navbar-link');
+    for (const link of links) {
+        link.classList.remove('link-active');
+    }
+    clickedElement.classList.add('link-active');
+}
+
+
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.scrollY + 400) >= document.body.offsetHeight && !scrollEnd) {
+        scrollEnd = true;
+        console.log('unten');
+        if(end < 100){
+            start = start + 10;
+            end = end + 10;
+            renderNews();
+        }  
+    }
+};
